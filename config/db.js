d89ae4a36c50dbda0eb8d007b9ba61b3a0ef167f
brotherilm <ilmanclasher@gmail.com>
2025-02-02 19:17:52 +0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,6 +1,13 @@
 import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
 
-const MONGODB_URI = "mongodb://localhost:27017/airdrop";
+dotenv.config();
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+if (!MONGODB_URI) {
+  throw new Error("MONGODB_URI is not defined in the environment variables");
+}
 
 let clientInstance = null;
 
@@ -23,7 +30,7 @@ export async function connectToDatabase() {
     // Store client instance
     clientInstance = client;
 
-    console.log("Connected to MongoDB on localhost");
+    console.log("Connected to MongoDB");
     return client;
   } catch (error) {
     console.error("MongoDB connection error:", error);
@@ -38,52 +45,3 @@ export async function closeDatabaseConnection() {
     clientInstance = null;
   }
 }
-
-// // db.js
-// import { MongoClient } from "mongodb";
-// import dotenv from "dotenv";
-
-// dotenv.config();
-
-// const MONGODB_URI = process.env.MONGODB_URI;
-
-// if (!MONGODB_URI) {
-//   throw new Error("MONGODB_URI is not defined in the environment variables");
-// }
-
-// let clientInstance = null;
-
-// export async function connectToDatabase() {
-//   // If we already have a client, return it
-//   if (clientInstance) {
-//     return clientInstance;
-//   }
-
-//   try {
-//     // Create a new client without deprecated options
-//     const client = new MongoClient(MONGODB_URI, {
-//       connectTimeoutMS: 10000,
-//       socketTimeoutMS: 45000,
-//     });
-
-//     // Establish connection
-//     await client.connect();
-
-//     // Store client instance
-//     clientInstance = client;
-
-//     console.log("Connected to MongoDB");
-//     return client;
-//   } catch (error) {
-//     console.error("MongoDB connection error:", error);
-//     throw error;
-//   }
-// }
-
-// export async function closeDatabaseConnection() {
-//   if (clientInstance) {
-//     await clientInstance.close();
-//     console.log("MongoDB connection closed");
-//     clientInstance = null;
-//   }
-// }
